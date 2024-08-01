fb011d9b9b1e6de9700e6413a3d00ccd7c2832e4
Christopher-OOC <olojedechristopher24@gmail.com>
2024-08-01 09:57:32 +0100

    commit

--- bin/count-words.dart
+++ bin/count-words.dart
@@ -1,8 +1,11 @@
 void main() {
-  filterCharacters('today');
+  Map<String, int?> filters =
+      filterCharacters('What is your name? My name is olojede christopher.');
+
+  print(filters);
 }
 
-Map<String, int>? filterCharacters(String paragraph) {
+Map<String, int?> filterCharacters(String paragraph) {
   Map<String, int?> returnMap = {};
 
   List<String> wordsArr = paragraph.split('');
@@ -13,12 +16,12 @@ Map<String, int>? filterCharacters(String paragraph) {
   for (int i = 0; i <= wordsArr.length - 1; i++) {
     if (currentChar == wordsArr[i]) {
       returnMap[currentChar] =
-          returnMap[currentChar] == null ? returnMap[currentChar]! + 1 : 1;
-    } else {
-      currentChar = wordsArr[i];
+          returnMap.containsKey(currentChar) ? returnMap[currentChar]! + 1 : 1;
+      if (i != wordsArr.length - 1) {
+        currentChar = wordsArr[i + 1];
+      }
     }
   }
 
-  print(returnMap);
-  return null;
+  return returnMap;
 }
